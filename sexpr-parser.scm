7a027034ecb98f1ee53d44e97cb1c36bf904e861
Alon Blumenreich <alon@mail.com>
2017-11-01 10:03:05 +0200

    The parser <Boolean> was successfully defined

--- sexpr-parser.scm
+++ sexpr-parser.scm
@@ -3,3 +3,18 @@
 (define <sexpr>
   ;; fill in the s-expression parser details here
   )
+
+(define <Boolean>
+    (new
+    	(*parser (word-ci "#t"))
+    	(*parser (word-ci "#f"))
+    	(*disj 2)
+    	(*pack (lambda (str)
+    		(let ((l (char-downcase (cadr str))))
+ 				(cond
+ 					((char=? l #\t) #t)
+ 					((char=? l #\f) #f)
+ 					(else (display "The value is no valid!"))))))
+
+    	done))
+
